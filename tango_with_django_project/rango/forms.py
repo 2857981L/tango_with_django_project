18bf7fc774990c4524cd11cf45db43aed971011b
“Yangbo <2857981L@student.gla.ac.uk>
2025-02-14 20:11:43 +0000

    Initial commit - Django project

---
+++ tango_with_django_project/rango/forms.py
@@ -0,0 +1,64 @@
+from django import forms
+from rango.models import Page, Category
+
+class CategoryForm(forms.ModelForm):
+    name = forms.CharField(max_length=128,
+                           help_text="Please enter the category name.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    slug = forms.CharField(widget=forms.HiddenInput(), required=False)
+
+    # An inline class to provide additional information on the form.
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Category
+        fields = ('name',)
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name', '').strip()
+        if not name:
+            raise forms.ValidationError("Category name cannot be empty!")
+        if Category.objects.filter(name__iexact=name).exists():
+            raise forms.ValidationError("This category already exists!")
+        return name
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['name'].widget.attrs['maxlength'] = Category._meta.get_field('name').max_length
+
+class PageForm(forms.ModelForm):
+    title = forms.CharField(max_length=128,
+                            help_text="Please enter the title of the page.")
+    url = forms.URLField(max_length=200,
+                         help_text="Please enter the URL of the page.")
+    views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+
+    class Meta:
+        # Provide an association between the ModelForm and a model
+        model = Page
+        # What fields do we want to include in our form?
+        # This way we don't need every field in the model present.
+        # Some fields may allow NULL values; we may not want to include them.
+        # Here, we are hiding the foreign key.
+        # we can either exclude the category field from the form,
+        exclude = ('category',)
+        # or specify the fields to include (don't include the category field).
+        fields = ('title', 'url', 'views')
+
+    def clean_url(self):
+        url = self.cleaned_data.get('url')
+        if url and not url.startswith(('http://', 'https://')):
+            url = f'http://{url}'
+        return url
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://',
+        # then prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+            
+        return cleaned_data
